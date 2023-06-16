708929027e8ab7328dbaa7c3ce95049566990b7c
sangjae84 <dltkdwo0514@gmail.com>
2023-06-16 11:32:58 +0900

    이미지와 비디오

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-# pip install streamlit
-# streamlit hello
-# ctrl + c : 실행 종료 (터미널)
 
+# ctrl + c : 실행 종료 (터미널)
 import streamlit as st  # streamlit -> import (가져오기) -> as (st 이름)
 # st라는 변수명으로 streamlit의 기능들을 사용하겠다
-
 # st. -> ctrl + space -> 다양한 기능(함수, 메소드)을 가지고 있다
-
 st.title("나의 파이썬 Streamlit 웹 페이지")
 st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
 st.write("내가 만든 streamlit 페이지, 너를 위해 구웠지")
-
 # 기능이 실행되는 순서대로 화면에서 표현
 st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")  # 유튜브 링크
 st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
 st.image("https://i.imgur.com/jorp5JH.png")  # 인터넷 링크
 # 여러 가지 옵션을 넣어서 세부 기능들을 차이
-st.image("image/image1.webp")  # 파일 경로 (app.py)
-#st.image(image="img/img.png") # 키워드를 사용해서...
-#st.image("img/img.png", use_column_width=True)  # 파일 경로 (app.py)
-st.image("image/image1.webp", width=100)  # 파일 경로 (app.py)
+st.image("img/img.png")  # 파일 경로 (app.py)
+st.image(image="img/img.png")  # 키워드를 사용해서...
+st.image("img/img.png", use_column_width=True)  # 파일 경로 (app.py)
+st.image("img/img.png", width=100)  # 파일 경로 (app.py)
 # https://imgur.com/
-# streamlit run app.py상
+
+# streamlit run app.py
+# streamlit run app.py
 
 # 제목 마크다운
 st.write("""
